acd6f77422a4f10c05cae8620777cd3f98443aae
Shanosha <wootenm1@gmail.com>
2026-02-19 05:10:36 -0500

    bugfix/removed unused import

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-const ThemeToggle = () => {
+function ThemeToggle() {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
   useEffect(() => {
